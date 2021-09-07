408557ffa4139e1c558fe5bb3e95b505fcb98132
tracy <tracywang1997@outlook.com>
2021-09-07 11:39:23 +0800

    完善serilog

--- Demo_Serilog/Program.cs
+++ Demo_Serilog/Program.cs
@@ -26,17 +26,67 @@ if (builder.Environment.IsDevelopment())
     app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Demo_Serilog v1"));
 }
 
-app.UseSerilogRequestLogging();
-
-app.MapGet("logname", (string name, Serilog.ILogger logger) =>
+/// <summary>
+/// 请求日志扩展
+/// </summary>
+app.UseSerilogRequestLogging(options =>
 {
-    logger.Debug(name);
-    logger.Information(name);
-    logger.Warning(name);
-    logger.Error(name);
-    logger.Fatal(name);
+    options.EnrichDiagnosticContext = (diagnosticContext, httpContext) =>
+    {
+        diagnosticContext.Set("Host", httpContext.Request.Host);
+        diagnosticContext.Set("Remote IP", httpContext.Connection.RemoteIpAddress);
+        diagnosticContext.Set("ContentType", httpContext.Response.ContentType);
+        diagnosticContext.Set("EndpointName", httpContext.GetEndpoint()?.DisplayName);
+    };
+});
 
-    return Results.Ok(name);
+/// <summary>
+/// 测试日志等级
+/// </summary>
+app.MapGet("log_level", (Serilog.ILogger logger) =>
+{
+    logger.Debug("debug");
+    logger.Information("information");
+    logger.Warning("warning");
+    logger.Error("error");
+    logger.Fatal("fatal");
+
+    return Results.Ok();
+});
+
+/// <summary>
+/// 结构化日志
+/// from https://github.com/serilog/serilog/wiki/Structured-Data
+/// </summary>
+app.MapGet("log_structured_data", (Serilog.ILogger logger) =>
+{
+    var count = 456;
+    logger.Information("Retrieved {Count} records", count);
+
+    var fruit = new[] { "Apple", "Pear", "Orange" };
+    logger.Information("In my bowl I have {Fruit}", fruit);
+
+    var fruitDic = new Dictionary<string, int> { { "Apple", 1 }, { "Pear", 5 } };
+    logger.Information("In my bowl I have {Fruit}", fruitDic);
+
+    var sensorInput = new { Latitude = 25, Longitude = 134 };
+    logger.Information("Processing {@SensorInput}", sensorInput);
+
+    //使用 $ 强制 ToString() , 记录的是 unknown.ToString()
+    var unknown = new[] { 1, 2, 3 };
+    logger.Information("Received {$Data}", unknown);
+});
+
+/// <summary>
+/// 注意事项
+/// </summary>
+app.MapGet("log_notice", (Serilog.ILogger logger) =>
+{
+    //使用模板减少内存消耗及提升性能 https://github.com/serilog/serilog/wiki/Writing-Log-Events#message-template-recommendations
+    // Don't:
+    Log.Information("The time is " + DateTime.Now);
+    // Do:
+    Log.Information("The time is {Now}", DateTime.Now);
 });
 
 app.Run();
