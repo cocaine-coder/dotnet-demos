66072fe0f4ed40f7e6b70de7da855324892a6395
tracy <tracywang1997@outlook.com>
2021-11-15 19:16:17 +0800

    添加hangfire demo

---
+++ Demo_Hangfire/Program.cs
@@ -0,0 +1,66 @@
+using Hangfire;
+using Hangfire.Common;
+using Hangfire.MemoryStorage;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddHangfire(configuration =>
+{
+    configuration
+        .SetDataCompatibilityLevel(CompatibilityLevel.Version_170)
+        .UseSimpleAssemblyNameTypeSerializer()
+        .UseRecommendedSerializerSettings()
+        .UseMemoryStorage();
+});
+
+builder.Services.AddHangfireServer();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+var summaries = new[]
+{
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+};
+
+app.UseHangfireDashboard();
+
+var backgroundJobs = app.Services.GetRequiredService<IBackgroundJobClient>();
+var recurringJobManager = app.Services.GetRequiredService<IRecurringJobManager>();
+backgroundJobs.Schedule(() => Console.WriteLine("Hello world from Hangfire!"),TimeSpan.FromSeconds(1));
+
+recurringJobManager.AddOrUpdate("showdate", Job.FromExpression(()=>Console.WriteLine(DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss"))), "*/1 * * * * ?");
+
+app.MapHangfireDashboard();
+
+app.MapGet("/weatherforecast", () =>
+{
+    var forecast = Enumerable.Range(1, 5).Select(index =>
+       new WeatherForecast
+       (
+           DateTime.Now.AddDays(index),
+           Random.Shared.Next(-20, 55),
+           summaries[Random.Shared.Next(summaries.Length)]
+       ))
+        .ToArray();
+    return forecast;
+})
+.WithName("GetWeatherForecast");
+
+app.Run();
+
+internal record WeatherForecast(DateTime Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
